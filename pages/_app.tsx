606f5da42b9856be7c3f2ae411f7a4f76107de52
unknown <nativzgo@gmail.com>
2022-02-02 12:22:29 +0100

    Basic look and functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { createClient, Provider } from 'urql';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+  body,div,p,ul,li {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  body{
+    background:#FEFBF3
+  }
+`;
+
+const client = createClient({
+  url: 'https://reasonapps-gql-api.vercel.app/api/graphql',
+});
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+};
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Provider value={client}>
+          <Component {...pageProps} />
+        </Provider>
+      </ThemeProvider>
+    </>
+  );
 }
-
-export default MyApp
